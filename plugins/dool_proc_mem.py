d9a2fb6906ef2488ca4f10be7e6ba0c8e105dc8c
George Schwarzman <g.schwarzman@hotmail.com>
2021-02-02 20:31:49 +0200

    Added proc-mem plugin for simple monitoring of process memory.

---
+++ plugins/dool_proc_mem.py
@@ -0,0 +1,26 @@
+### Author: George Shvartsman <stinger.xiii@gmail.com>
+### Modified from dool-mem: Dag Wieers <dag$wieers,com>
+
+class dstat_plugin(dstat):
+    """
+    Process allocated memory size. 
+    
+    Shows memory info of process defined by DOOL_MONITORED_PID environment variable.
+    """
+
+    def __init__(self):
+        monitored_pid = str(os.environ.get('DOOL_MONITORED_PID'))
+        self.name = 'pid ' + monitored_pid + ' memory'
+        self.nick = ('VIRT', 'RES', 'SHR')
+        self.vars = ('virtual', 'resident', 'shared')
+        self.type = 'd'
+        self.open('/proc/%s/statm' % monitored_pid)
+
+    def extract(self):
+        l = self.splitline()
+        self.val['virtual'] = int(l[0]) * pagesize / 1024
+        self.val['resident'] = int(l[1]) * pagesize / 1024
+        self.val['shared'] = int(l[2]) * pagesize / 1024
+
+
+    
